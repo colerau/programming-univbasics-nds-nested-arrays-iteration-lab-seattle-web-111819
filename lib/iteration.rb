abf40f4cc909036a61c25f1a77414ef672d46e4a
Cole Rau <colevrau@gmail.com>
2019-10-22 00:45:15 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -31,7 +31,7 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-  src.reduce do |memo, element|
+  src.reduce(0) do |memo, element|
     if (element[0] % 2 == 0)
       if (element[1] % 2 == 0) 
         element[0] + element[1]
